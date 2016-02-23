3cb04ce68bda51787723b99a93c3ec309821a632
bkopilov <bkopilov@redhat.com>
2016-02-23 14:40:08 +0200

    node list

--- common.py
+++ common.py
@@ -3,6 +3,7 @@ def source_stackrc():
     return "source /home/stack/stackrc"
 
 def get_undercloud_nodes(ssh):
-    cmd = source_stackrc() + "&& " + "nova list"
+    nova_list = """nova list | awk '{print $4 " " $12}' | sed  's/=/ /g' """
+    cmd = source_stackrc() + "&& " + nova_list
     nodes = ssh.send_cmd(cmd)
     print nodes
