5cb561833f68abe2009003dcd7b0b3145d0bdbb7
bkopilov <bkopilov@redhat.com>
2016-02-25 08:04:10 +0200

    log collect tempest

--- common.py
+++ common.py
@@ -270,7 +270,7 @@ class UnderCloud(object):
     def collect_testr_tests(self, ssh, local_dest_dir):
         ssh_conn = ssh.get_connection()
         sftp = ssh_conn.open_sftp()
-        remote_xunit_file = os.path.join(self.TEMPEST_DIR, "xunit_temp.xml")
+        remote_xunit_file = os.path.join(self.TEMPEST_DIR + "/tempest", "xunit_temp.xml")
         local_xunit_file = os.path.join(local_dest_dir, "xunit_temp.xml")
         sftp.get(remote_xunit_file, local_xunit_file)
         sftp.close()
